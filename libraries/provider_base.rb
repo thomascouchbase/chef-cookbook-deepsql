e987f0942c69c39fc280d7358c7480e9e2a4b138
Robert Buck <buck.robert.j@gmail.com>
2016-04-06 13:21:45 -0400

    revert my.cnf removal

--- libraries/provider_base.rb
+++ libraries/provider_base.rb
@@ -41,7 +41,7 @@ class Chef
 
         create_group_and_user
 
-        delete_existing_my_cnf
+        # delete_existing_my_cnf
 
         create_support_directories
 
@@ -50,15 +50,15 @@ class Chef
         initialize_database
       end
 
-      def delete_existing_my_cnf
-        # Turns out that mysqld is hard coded to try and read
-        # /etc/mysql/my.cnf, and its presence causes problems when
-        # setting up multiple services.
-        file "#{new_resource.name} :create /etc/mysql/my.cnf" do
-          path '/etc/mysql/my.cnf'
-          action :delete
-        end
-      end
+      # def delete_existing_my_cnf
+      #   # Turns out that mysqld is hard coded to try and read
+      #   # /etc/mysql/my.cnf, and its presence causes problems when
+      #   # setting up multiple services.
+      #   file "#{new_resource.name} :create /etc/mysql/my.cnf" do
+      #     path '/etc/mysql/my.cnf'
+      #     action :delete
+      #   end
+      # end
 
       def initialize_database
         bash "#{new_resource.name} :create initial records" do
